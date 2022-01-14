8ff9f9c903d220cd617f025d901acc52db742ada
Pedro Henrique <pedrohrds05@hotmail.com>
2022-01-14 13:58:47 -0300

    Para o pull

---
+++ src/components/CardsNavegation.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/mouse-events-have-key-events */
+import PropTypes from 'prop-types';
+import React from 'react';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+function CardsNavegation({ image }) {
+  return (
+    <nav className="container-icons">
+      <a
+        href={image}
+        target="_blank"
+        rel="noreferrer"
+        className="icons-projects"
+        title="Ir para o repositório do projeto"
+      >
+        <GitHubIcon style={{ fontSize: '2.5em' }} />
+      </a>
+      <a
+        href={image}
+        target="_blank"
+        rel="noreferrer"
+        className="icons-projects"
+        title="Ir para o deploy"
+      >
+        <ArrowForwardIcon style={{ fontSize: '2.5em' }} />
+      </a>
+    </nav>
+  );
+}
+
+CardsNavegation.propTypes = {
+  image: PropTypes.string.isRequired,
+};
+
+export default CardsNavegation;
