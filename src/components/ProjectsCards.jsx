859f813513f37009948398308a213727f278d665
Pedro Henrique <pedrohrds05@hotmail.com>
2022-02-11 22:32:57 -0300

    Adicionando datatestid para os testes

--- src/components/ProjectsCards.jsx
+++ src/components/ProjectsCards.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CardsNavegation from './CardsNavegation';
 
-function ProjectsCards({ title, image, content, repository, deploy }) {
+function ProjectsCards({ title, image, content, repository, deploy, testId }) {
   return (
     <section className="cards-projects">
       <h1 className="title-project">{title}</h1>
       <img src={ image } alt="Imagem do projeto" />
-      <p className="project-description">{content}</p>
+      <p className="project-description" data-testid={ testId }>{content}</p>
       <CardsNavegation repository={ repository } deploy={ deploy } />
     </section>
   );
@@ -19,6 +19,7 @@ ProjectsCards.propTypes = {
   deploy: PropTypes.string.isRequired,
   repository: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  testId: PropTypes.string.isRequired,
 };
 
 export default ProjectsCards;
